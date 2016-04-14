ed0620fd760664105e91b10796cb03bb8b0dffc8
panyin0706 <n8327912nn@yahoo.com.tw>
2016-04-14 12:04:48 +0800

    Added files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+ <html>
+	<head>
+		<title>
+			Register
+		</title>
+	</head>
+	<body>
+
+
+
+	<table border="1" name="uploadurl">
+	<tbody>
+	<tr><td>名稱</td><td>網址路徑</td><td>短網址</td><td>上傳時間</td><td>瀏覽人次</td></tr>
+	</tbody>
+	</table>
+
+	</table>
+	</body>
